2f327876afc48c3a311cae19d0c28fb836179475
Karumbaiah K G <gauravzz1@gmail.com>
2014-01-26 21:19:34 +0530

    removed unwanted files
    removed unwanted files

--- modules/week1.py
+++
@@ -1,1 +0,0 @@
-print "week1"
--- userSetup.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-print sys.path
-
-sys.path.append("C:/Users/Ganapathi K A/Documents/GitHub/Master")
-sys.path.append(os.environ['RDOJO'])
-sys.path.append('R:')
