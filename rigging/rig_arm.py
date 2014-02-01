4f299867cda34f2f7ba90cc9783af7fc65c4046a
samonty <sa.a.montgomery@gmail.com>
2014-02-01 01:05:54 -0600

    rig_arm.py
    added name variables

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -1,11 +1,18 @@
 import maya.cmds as cmds
 
-cmds.joint(p=(0, 14.311348, 0.0477619))
-cmds.joint(p=(0, 8.52323, -2.195134))
-cmds.joint('joint1', e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(p=(0, 1.613664, -0.422523))
-cmds.joint('joint2', e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(p=(0, 0.492216, 0.373344))
-cmds.joint('joint3', e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(p=(0, -0.737759, 1.060683))
-cmds.joint('joint4', e=True, zso = True, oj = 'xyz', sao = 'yup')
+shoulder = "shoulder_jnt"
+elbow = "elbow_jnt"
+wrist = "wrist_jnt"
+palm = "palm_jnt"
+finger = "finger_jnt"
+
+
+cmds.joint(n= shoulder, p=(0, 14.311348, 0.0477619))
+cmds.joint(n = elbow, p=(0, 8.52323, -2.195134))
+cmds.joint(shoulder, e=True, zso = True, oj = 'xyz', sao = 'yup')
+cmds.joint(n=wrist, p=(0, 1.613664, -0.422523))
+cmds.joint(elbow, e=True, zso = True, oj = 'xyz', sao = 'yup')
+cmds.joint(n=palm, p=(0, 0.492216, 0.373344))
+cmds.joint(wrist, e=True, zso = True, oj = 'xyz', sao = 'yup')
+cmds.joint(n=finger, p=(0, -0.737759, 1.060683))
+cmds.joint(palm, e=True, zso = True, oj = 'xyz', sao = 'yup')
