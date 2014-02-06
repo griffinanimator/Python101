f7460be44f378f12bfffe8a739c7a6aa89ea29dc
samonty <sa.a.montgomery@gmail.com>
2014-02-06 17:37:55 -0600

    init and rig_arm update
    added init python files and updated rig_arm to match this week's
    assignment

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -4,9 +4,26 @@ shoulder = "shoulder_jnt"
 elbow = "elbow_jnt"
 wrist = "wrist_jnt"
 palm = "palm_jnt"
-finger = "finger_jnt"
+
+loc_info = ([shoulder, [0, 14.311348, 0.0477619]], [elbow, [0, 8.52323, -2.195134]], [wrist,[0, 1.613664, -0.422523]], [palm, [0, 0.492216, 0.373344]])
+                                                                                      
+                                                                                      
+for loc in loc_info:
+    print loc
+    locator = cmds.spaceLocator(n=loc[0], p=loc[1]) 
+    print arm                                                                                     
+
+#cmds.spaceLocator(p=(0,0,0))
 
 
+
+
+
+
+
+
+
+'''
 cmds.joint(n= shoulder, p=(0, 14.311348, 0.0477619))
 cmds.joint(n = elbow, p=(0, 8.52323, -2.195134))
 cmds.joint(shoulder, e=True, zso = True, oj = 'xyz', sao = 'yup')
@@ -15,4 +32,11 @@ cmds.joint(elbow, e=True, zso = True, oj = 'xyz', sao = 'yup')
 cmds.joint(n=palm, p=(0, 0.492216, 0.373344))
 cmds.joint(wrist, e=True, zso = True, oj = 'xyz', sao = 'yup')
 cmds.joint(n=finger, p=(0, -0.737759, 1.060683))
-cmds.joint(palm, e=True, zso = True, oj = 'xyz', sao = 'yup')
+cmds.joint(palm, e=True, zso = True, oj = 'xyz', sao = 'yup')'''
+
+#put names in a list
+#have another list for locators
+#loop through list of locators
+#assign name to locator via index num
+
+#joints, names, positions
