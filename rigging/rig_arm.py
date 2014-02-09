639f27940e8f128e22fba71f40e17f1ca8617642
samonty <sa.a.montgomery@gmail.com>
2014-02-08 19:45:33 -0600

    rig_arm and json file
    Updated rig_arm to reflect assignment in syllabus. Added JSON file for
    dictionary

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -1,42 +1,28 @@
-import maya.cmds as cmds
+import maya.cmds as cmds, json, io
 
-shoulder = "shoulder_jnt"
-elbow = "elbow_jnt"
-wrist = "wrist_jnt"
-palm = "palm_jnt"
+arm_name = { 
+'shoulder': 'lctr_l_arm1', 
+'elbow': 'lctr_l_arm2', 
+'wrist' : 'lctr_l_wrist', 
+'palm' : 'lctr_l_armEnd'} #dictonary to store locator names
+  
+ 
+loc_info = {
+arm_name['shoulder']: (0, 14.311348, 0.0477619), 
+arm_name['elbow']: (0, 8.52323, -2.195134), 
+arm_name['wrist']:(0, 1.613664, -0.422523), 
+arm_name['palm']: (0, 0.492216, 0.373344)
 
-loc_info = ([shoulder, [0, 14.311348, 0.0477619]], [elbow, [0, 8.52323, -2.195134]], [wrist,[0, 1.613664, -0.422523]], [palm, [0, 0.492216, 0.373344]])
+} #dictionary to store locator locations
+
+fout = open("loc_info.json", 'w')
+json.dump(loc_info, fout, indent=3)
+fout.close()
+
+                                                                                     
                                                                                       
-                                                                                      
-for loc in loc_info:
-    print loc
-    locator = cmds.spaceLocator(n=loc[0], p=loc[1]) 
-    print arm                                                                                     
-
-#cmds.spaceLocator(p=(0,0,0))
-
-
-
-
-
-
-
-
-
-'''
-cmds.joint(n= shoulder, p=(0, 14.311348, 0.0477619))
-cmds.joint(n = elbow, p=(0, 8.52323, -2.195134))
-cmds.joint(shoulder, e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(n=wrist, p=(0, 1.613664, -0.422523))
-cmds.joint(elbow, e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(n=palm, p=(0, 0.492216, 0.373344))
-cmds.joint(wrist, e=True, zso = True, oj = 'xyz', sao = 'yup')
-cmds.joint(n=finger, p=(0, -0.737759, 1.060683))
-cmds.joint(palm, e=True, zso = True, oj = 'xyz', sao = 'yup')'''
-
-#put names in a list
-#have another list for locators
-#loop through list of locators
-#assign name to locator via index num
-
-#joints, names, positions
+for loc in loc_info: #builds the locator according to the information in the loc_info dictionary
+  
+    locator = cmds.spaceLocator(n=loc, p=loc_info[loc]) 
+    print loc  #prints locator name from the dictionary
+    print loc_info[loc]  #prints locator position from the dictionary                                                                                
