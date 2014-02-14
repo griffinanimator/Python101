4cf60e50b5e70b2575e43dd655dbfdd017d58996
woshodi <fushachicken@gmail.com>
2014-02-14 12:17:05 -0600

    Update rig_arm.py

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -1,51 +1,61 @@
-shoulder=cmds.joint(name='shoulder_jnt', position=[5,10,-4])
-arm=cmds.joint(name='forarm_jnt01', position=[3.5,10,-11])
-wrist=cmds.joint(name='wrist_jnt', position=[3.5,9.5, -19])
-handEnd=cmds.joint(name='hand_end_jnt', position=[3.5,9.5, -23])
-cmds.select('wrist_jnt')
-cmds.duplicate('wrist_jnt', name='fingerOne_jnt')
-cmds.parent( world=True)
-cmds.rotate(0,-20,-20)
-cmds.duplicate(name='fingerTwo_jnt')
-cmds.rotate(0,20,-28)
-cmds.parent('fingerTwo_jnt', 'fingerOne_jnt', 'wrist_jnt')
-cmds.ikHandle(n='rpIk_l_arm01', solver= 'ikRPsolver', startJoint='shoulder_jnt', endEffector='wrist_jnt')
-box=cmds.curve( p=[(1.5,1.5,1.5),(1.5,1.5,-1.5), (-1.5,1.5,-1.5), (-1.5,1.5,1.5), (1.5,1.5,1.5), (1.5,-1.5,1.5),
+#Creates a dictionary
+arm_info={}
+#Creates list that goes in a dictionary that defines placement. Note when creating points think of how they will look placed on a grid.
+jntlytPlmt=(['ljnt_shldr', [0.0,0.0,2.0]], ['ljnt_elbow', [0.0,2.0,4.0]], ['ljnt_wrist', [0.0,0.0,7.0]])
+#Build joints with a list
+ljntList=[]
+for jnt in jntlytPlmt:
+    #create a name for the command to represent the making of joints
+    jntlyt=cmds.joint(name=jnt[0], position=jnt[1])
+    #Add jntlyt which holds the placement
+    ljntList.append(jntlyt)
+print ljntList
+#Next another key will be added to the list. The arm_info is the dictionary or house every else goes in it.
+arm_info['LjntInfo']=ljntList
+cmds.select(all=True)
+cmds.move(10,10,10)
+#The layout was used to put items in the scene 
+#Create an empty list
+jntInfo=[]
+cmds.select(deselect=True)
+for ljnt in arm_info['LjntInfo']:
+    #change name to a string
+    jntName= ljnt.replace('ljnt', 'jnt')
+    #Grabs positions of ljnt which not yet determined and places new joints from list in exact spot. This is for jnt placement
+    PosJnt=cmds.xform(ljnt, query=True, worldSpace=True, translation=True)
+    cmds.joint(name=jntName, position=PosJnt)
+    jntInfo.append(jntName)
+arm_info['jntInfo']=jntInfo
+
+#Make IK
+ikH=cmds.ikHandle(n='rp_lArm', solver='ikRPsolver', startJoint=arm_info['jntInfo']
+[0], endEffector=arm_info['jntInfo'][2])
+arm_info['IKH']=ikH
+#Now hide joints for layout
+cmds.setAttr('ljnt_shldr.visibility',0)
+cmds.setAttr('ljnt_elbow.visibility',0)
+cmds.setAttr('ljnt_wrist.visibility',0)
+#Make Control
+arm_info['ikControl']=cmds.curve( p=[(1.5,1.5,1.5),(1.5,1.5,-1.5), (-1.5,1.5,-1.5), (-1.5,1.5,1.5), (1.5,1.5,1.5), (1.5,-1.5,1.5),
 (1.5,-1.5,-1.5), (1.5,1.5,-1.5), (-1.5,1.5,-1.5), (-1.5,-1.5,-1.5), (1.5,-1.5,-1.5),
 (1.5,1.5,-1.5), (-1.5,1.5,-1.5),(-1.5,-1.5,-1.5), (-1.5,-1.5,1.5), (-1.5,1.5,1.5), 
 (-1.5,1.5,-1.5), (1.5,1.5,-1.5), (1.5,1.5,1.5), (-1.5,1.5,1.5), (-1.5,-1.5,1.5),
 (1.5,-1.5,1.5), (1.5,1.5,1.5)], d=1 )
-cmds.rename(box, 'anim_l_wrist01')
-cmds.parent('anim_l_wrist01', 'wrist_jnt')
-cmds.setAttr("anim_l_wrist01.translateZ", 0)
-cmds.setAttr("anim_l_wrist01.translateX", 0)
-cmds.setAttr("anim_l_wrist01.translateY", 0)
-cmds.scale(0.6, 0.6, 0.6)
-cmds.parent('anim_l_wrist01', w=True)
+cmds.rename(arm_info['ikControl'], 'anim_l_wrist01')
+#This snaps control at wrist and freezes transformations
+ikPos=cmds.xform('rp_lArm', q=True, ws=True, translation=True)
+cmds.xform("anim_l_wrist01", ws=True, translation=ikPos)
+cmds.scale(0.2, 0.2, 0.2)
 cmds.makeIdentity(apply=True)
-cmds.spaceLocator(n='wrist_loc')
-wristPos=cmds.xform("wrist_jnt", q=True, worldSpace=True, translation=True )
-cmds.xform('wrist_loc', worldSpace=True, translation=wristPos)
-cmds.duplicate('anim_l_wrist01', n='anim_l_elbow01')
-elbowPos=cmds.xform("forarm_jnt01", q=True, worldSpace=True, translation=True)
-cmds.select('anim_l_elbow01')
-cmds.move(3.5,10,-11, rpr=True)
-cmds.move(-5,0,0, r=True)
-cmds.scale(0.5, 0.5, 0.5)
-cmds.makeIdentity(apply=True)
-cmds.parentConstraint('wrist_loc', 'wrist_jnt')
-cmds.parent('wrist_loc', 'anim_l_wrist01' )
-cmds.parent('rpIk_l_arm01', 'wrist_loc')
-cmds.select('anim_l_elbow01')
-cmds.addAttr(shortName='Extra_Controls', longName='Extra_Controls', at='double') 
-cmds.setAttr('anim_l_elbow01.Extra_Controls', cb=True )
-cmds.setAttr('anim_l_elbow01.rx', lock=True, keyable=False, channelBox=False)
-cmds.setAttr('anim_l_elbow01.ry', lock=True, keyable=False, channelBox=False)
-cmds.setAttr('anim_l_elbow01.rz', lock=True, keyable=False, channelBox=False)
-cmds.setAttr('anim_l_elbow01.sx', lock=True, keyable=False, channelBox=False)
-cmds.setAttr('anim_l_elbow01.sy', lock=True, keyable=False, channelBox=False)
-cmds.setAttr('anim_l_elbow01.sz', lock=True, keyable=False, channelBox=False)
-cmds.poleVectorConstraint('anim_l_elbow01', 'rpIk_l_arm01')
+#Make a locator at ik
+loc=cmds.spaceLocator(name='lctrl')
+cmds.xform("lctrl", ws=True, translation=ikPos)
+cmds.parent('rp_lArm', 'lctrl')
+cmds.parent('lctrl', 'anim_l_wrist01')
+
+
+
+
 
 
 
@@ -53,3 +63,5 @@ cmds.poleVectorConstraint('anim_l_elbow01', 'rpIk_l_arm01')
 
 
    
+
+
