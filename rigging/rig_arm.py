f9dc355512f93e63ab6021b4444ab52a6f5a177b
samonty <sa.a.montgomery@gmail.com>
2014-02-13 19:51:04 -0600

    json, rig_arm, startup, init
    Moved/directed JSON to proper location. Added init's to all folders,
    updated rig_arm to reflect assignment, and created startup.py

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -14,15 +14,22 @@ arm_name['wrist']:(0, 1.613664, -0.422523),
 arm_name['palm']: (0, 0.492216, 0.373344)
 
 } #dictionary to store locator locations
+ 
+                                                                                 
 
-fout = open("loc_info.json", 'w')
-json.dump(loc_info, fout, indent=3)
-fout.close()
+def build_Locators(loc_dict):                                                                                      
+    for loc in loc_info: #builds the locator according to the information in the loc_info dictionary
+      
+        locator = cmds.spaceLocator(n=loc, p=loc_info[loc]) 
+        print loc  #prints locator name from the dictionary
+        print loc_info[loc]  #prints locator position from the dictionary
+     
+    write_JSON(loc_dict)
+        
 
-                                                                                     
-                                                                                      
-for loc in loc_info: #builds the locator according to the information in the loc_info dictionary
-  
-    locator = cmds.spaceLocator(n=loc, p=loc_info[loc]) 
-    print loc  #prints locator name from the dictionary
-    print loc_info[loc]  #prints locator position from the dictionary                                                                                
+def write_JSON(loc_dict):
+    fout = open("C:\Users\Sarah\Documents\GitHub\Python101\data\loc_info.json", 'w') #figure out file directories
+    json.dump(loc_info, fout, indent=3)
+    fout.close()                                                                                
+    
+build_Locators(loc_info) 
