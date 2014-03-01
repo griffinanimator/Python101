ed74318083a622040f62dc0bb3030b2a74a9bfb3
samonty <sa.a.montgomery@gmail.com>
2014-02-28 19:35:24 -0600

    arm fk done. ik wip

--- rigging/rig_arm.py
+++ rigging/rig_arm.py
@@ -1,6 +1,8 @@
 import maya.cmds as cmds, json, io   
                                                                 
 
+
+
     
 arm_name = { 
 'shoulder': 'lctr_l_arm1', 
@@ -16,6 +18,8 @@ def build_Locators():
     for key in data: #builds the locator according to the information in the loc_info dictionary
         locator = cmds.spaceLocator(n=key)[0] #creates the locator at world origin. Avoided using the position(p) flag becuause it positioned according to the local position, not translation
         cmds.xform(locator, t=data[key]) #changes translation of locator according to info in JSON
+    build_Joints()
+    
     
 def get_Data():
     
@@ -44,26 +48,77 @@ def write_JSON(arm_name):
    
 
 def build_Joints():
-    
+    jnt = []
     position = get_Data()
     cmds.select(d=True) #deselects locator so joints won't be parented to it
     for key in sorted(position): #sorted so joints will be in correct order
         name = key.replace('lctr','jnt')
         locPos = cmds.xform(key, q=True, t=True, ws = True) #gets locator position
-        test= cmds.joint(n=name, p=locPos) #creates joints in locator's position
+        buildJnt= cmds.joint(n=name, p=locPos) #creates joints in locator's position
+        jnt.append(buildJnt)
         cmds.delete(key) #deletes locator
+    
+    for i  in range(len(jnt)):
+        cmds.joint(jnt[i], e=True, zso = True, oj = 'xyz')
+ 
+    build_Ik(jnt)
+    
        
       
+def build_Fk(jnt):
+    fkctrl = []
+    fkgroup = []
+    cmds.select(d=True) 
+    for i in range(len(jnt)-1): # creates ctrls for all joints but the palm
+        jntpos = cmds.xform(jnt[i], q=True, t=True, ws = True)
+        name = jnt[i].replace('jnt', 'ctrl_fk')
+        circle = cmds.circle(n=name, nr= (1,0,0), r = 1)
+        cmds.xform(circle, t=jntpos)
+        grp = cmds.group(circle, n ="grp_" + name) #groups for controls
+        cmds.parentConstraint(jnt[i], grp, mo=False) #temporary parent constraint to get right angle for control
+        cmds.parentConstraint(jnt[i], grp, e=True, rm=True)
+         
+        fkgroup.append(grp)
+        cmds.parentConstraint(circle, jnt[i])
+        
+        fkctrl.append(circle)
+        
+    for i in range(1, len(fkctrl)): #parent const ctrls to the upper in heirarchy for all controls but the first one
+        cmds.parentConstraint(fkctrl[i-1], fkgroup[i], mo = True)
+        
+         
+def build_Ik(jnt): 
+    ikrpName= jnt[0].replace('jnt', 'ikRP')
+    ikname = jnt[0].replace('jnt', 'ctrl_ik') 
+    wristjnt = jnt[len(jnt)-2]
     
-       
+    jntpos = cmds.xform(jnt[len(jnt)-2], q=True, t=True, ws = True)
+    
+    ikrp = cmds.ikHandle(n=ikrpName, sj = jnt[0], ee= wristjnt)
+    
+    #create control
+    ikcon = cmds.circle(n=ikname, nr= (1,0,0), r = 1.5)
+    cmds.xform(ikname, t=jntpos)
+    grp =  cmds.group(ikname, n ="grp_" + ikname)
+    cmds.parentConstraint(wristjnt, grp, mo=False)
+    cmds.parentConstraint(wristjnt, grp, e=True, rm=True)
+    
+    cmds.pointConstraint(ikcon, ikrpName)
+
+    cmds.orientConstraint(ikcon, wristjnt)
+    
+    #polevector const
+    
+        
     
 
-#build_Locators()        
-#build_Joints()
+build_Locators() 
+#build_Joints()       
+
      
     
-#def build_Ik():
+
     
     
-#def build_Fk():
+
                                                                             
