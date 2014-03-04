1280ebd7aedaa77fc6114150c6e8e8d524ab293f
woshodi <fushachicken@gmail.com>
2014-03-04 09:17:15 -0600

    Create json_utils.py

---
+++ utils/json_utils.py
@@ -0,0 +1,13 @@
+filename='C:/Users/Family/Documents/GitHub/Python101/data/locator_info.json'
+
+def writeJson(fileName, data):
+	with open(fileName, 'w') as outfile:
+		json.dump(data, outfile)
+
+	file.close(outfile)
+
+def readJson(fileName):
+	with open(fileName, 'r') as infile:
+		data=(open(infile.name, 'r').read())
+	return data
+	
