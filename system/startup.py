4ee5caf9a9fae8a72a6d1f96beca87ff6c9abc65
woshodi <fushachicken@gmail.com>
2014-03-10 12:11:58 -0500

    Update startup.py

--- system/startup.py
+++ system/startup.py
@@ -1,9 +1,17 @@
-print "Hello"
 import maya.cmds as cmds
 
 cmds.upAxis( ax='y', rv=True )
 cmds.currentUnit( linear='cm' )
 cmds.currentUnit( time='ntsc' )
+
+
+def layOutButton(*args):
+	import system.practice_ui as practice_ui
+	reload(practiceui)
+	practiceui=practiceui.Practice_UI()
+	practiceui.ui()
+
+
 
 def createMenu(*args):
 	mi = cmds.window('MayaWindow', ma=True, q=True)
@@ -12,5 +20,8 @@ def createMenu(*args):
 			cmds.deleteUI('RDojo_Menu', m=True)
 
 	cmds.menu('RDojo_Menu', label='RDMenu', to=True, p='MayaWindow')
+	cmds.menuItem('RiggingTools', c=layOutButton)
+
 
 createMenu()
+	
