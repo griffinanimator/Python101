fb72c5d21250423ea5092a767beead2a089ec001
Karumbaiah K G <gauravzz1@gmail.com>
2014-03-10 08:37:36 +0530

    utils and usersetup.py
    utils and usersetup.py changes

--- userSetup.py
+++ userSetup.py
@@ -1,9 +1,9 @@
 import os
 import sys
-
+-	
 print sys.path
 import maya.cmds as cmds
-
+	
 sys.path.append('C:/Users/Ganapathi K A/Documents/GitHub/Python101')
-
+	
 cmds.evalDeferred('import system.startup')
